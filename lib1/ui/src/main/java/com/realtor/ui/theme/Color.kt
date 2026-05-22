9a9b6f630528408e8d161be121b9705ec67c42f9
preethasuraj <preetha.ng@gmail.com>
2026-05-22 13:16:00 -0500

    Delete lib1 directory

--- lib1/ui/src/main/java/com/realtor/ui/theme/Color.kt
+++
@@ -1,9 +0,0 @@
-package com.realtor.ui.theme
-
-import androidx.compose.ui.graphics.Color
-
-internal val RealtorRed = Color(0xFFCC0000 /*0xFF0066FF*/)
-internal val RealtorRedDark = Color(0xFF8B0000)
-internal val RealtorSand = Color(0xFFF5EFE6)
-internal val RealtorInk = Color(0xFF1A1A1A)
-internal val RealtorSlate = Color(0xFF4A4A4A)
