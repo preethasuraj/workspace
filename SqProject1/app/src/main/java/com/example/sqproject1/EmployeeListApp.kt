ea0cb7723e0f4a0ef8311992666bdcccb6b180d3
preethasuraj <preetha.ng@gmail.com>
2026-05-22 13:13:07 -0500

    Delete SqProject1 directory

--- SqProject1/app/src/main/java/com/example/sqproject1/EmployeeListApp.kt
+++
@@ -1,7 +0,0 @@
-package com.example.sqproject1
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class EmployeeListApp: Application()
--- SqProject1/app/src/main/java/com/example/sqproject1/network/EmployeeApi.kt
+++
@@ -1,9 +0,0 @@
-package com.example.sqproject1.network
-
-import retrofit2.http.GET
-
-interface EmployeeApi {
-
-    @GET("employees.json")
-    suspend fun getEmployees() : EmployeeResponse
-}
