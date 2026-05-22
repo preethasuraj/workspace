8a41d85ae507915b66a2ee5b1721e5f93fe056bd
preethasuraj <preetha.ng@gmail.com>
2026-05-22 13:10:45 -0500

    Delete MyApplication5 directory

--- MyApplication5/app/src/main/java/com/example/myapplication/database/CartDatabase.kt
+++
@@ -1,9 +0,0 @@
-package com.example.myapplication.database
-
-import androidx.room.Database
-import androidx.room.RoomDatabase
-
-@Database(entities = [CartEntity::class, Product::class], version = 1)
-abstract class CartDatabase(): RoomDatabase() {
-    abstract fun cartDao(): CartDao
-}
