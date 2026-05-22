6fa0c79aa505af254443a3c21a416b4ad21b18f0
preethasuraj <preetha.ng@gmail.com>
2026-05-22 13:14:36 -0500

    Delete Test5 directory

--- Test5/app/src/main/README.md
+++
@@ -1,7 +0,0 @@
-Property List
-    - scrollable list
-    - handle error and empty states
-    - unit test
-
-Out of scope:
-    - offline, refresh/retry
--- Test5/app/src/main/java/com/example/test5/PropertyListApp.kt
+++
@@ -1,8 +0,0 @@
-package com.example.test5
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class PropertyListApp: Application() {
-}
