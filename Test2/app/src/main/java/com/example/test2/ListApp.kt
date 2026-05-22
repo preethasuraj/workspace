1d0b1bc8da9e18ab2a7186022789cb60d8a58588
preethasuraj <preetha.ng@gmail.com>
2026-05-22 13:13:51 -0500

    Delete Test2 directory

--- Test2/app/src/main/java/com/example/test2/ListApp.kt
+++
@@ -1,7 +0,0 @@
-package com.example.test2
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class ListApp: Application()
--- Test2/app/src/main/java/com/example/test2/network/PropertyService.kt
+++
@@ -1,8 +0,0 @@
-package com.example.test2.network
-
-import retrofit2.http.GET
-
-interface PropertyService {
-    @GET("employees.json")
-    suspend fun getProperties(): PropertyResponse
-}
