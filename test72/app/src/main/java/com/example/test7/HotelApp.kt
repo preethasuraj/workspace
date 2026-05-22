54b0e9a26ca613a04619ec7dd91671ef9ee0017d
preethasuraj <preetha.ng@gmail.com>
2026-05-22 13:16:36 -0500

    Delete test72 directory

--- test72/app/src/main/java/com/example/test7/HotelApp.kt
+++
@@ -1,8 +0,0 @@
-package com.example.test7
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class HotelApp: Application() {
-}
--- test72/app/src/main/java/com/example/test7/network/NetworkApi.kt
+++
@@ -1,9 +0,0 @@
-package com.example.test7.network
-
-import retrofit2.http.GET
-
-interface NetworkApi {
-
-    @GET("aa4e2939-c546-43f8-bd71-32acdee83b9d")
-    suspend fun getHotels(): HotelResponse
-}
