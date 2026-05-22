ecbf1fc0d2a42de9fdfd30810fad014d2caffd06
preethasuraj <preetha.ng@gmail.com>
2026-05-22 13:14:52 -0500

    Delete Test6 directory

--- Test6/app/src/main/README.md
+++
@@ -1,9 +0,0 @@
-Property List
--Scrollable list
-- Account for error/empty
-- display the photo in the list
-- unit test
-
----Out of scope
--offline
--retry/refresh
--- Test6/app/src/main/java/com/example/test6/network/PropertyService.kt
+++
@@ -1,8 +0,0 @@
-package com.example.test6.network
-
-import retrofit2.http.GET
-
-interface PropertyService {
-    @GET("hotels")
-    suspend fun fetchProperties(): PropertyResponse
-}
