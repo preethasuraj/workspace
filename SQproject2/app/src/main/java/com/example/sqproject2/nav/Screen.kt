5ae51a65f5c012edfee50f2c8509fff95b9dda22
preethasuraj <preetha.ng@gmail.com>
2026-05-22 13:12:59 -0500

    Delete SQproject2 directory

--- SQproject2/app/src/main/java/com/example/sqproject2/nav/Screen.kt
+++
@@ -1,7 +0,0 @@
-package com.example.sqproject2.nav
-import kotlinx.serialization.Serializable
-@Serializable
-sealed class Screen(val route: String) {
-
-    data object EmployeeListScreen: Screen("Employee List")
-}
--- SQproject2/app/src/main/java/com/example/sqproject2/network/EmployeeApi.kt
+++
@@ -1,8 +0,0 @@
-package com.example.sqproject2.network
-
-import retrofit2.http.GET
-
-interface EmployeeApi {
-    @GET("employees.json")
-    suspend fun getEmployees(): EmployeeList
-}
